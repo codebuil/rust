60178388b520a8a753d92f1362e3d768effafbb7
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2022-09-28 17:04:35 +0100

    integer 128 bits

---
+++ int128.rs
@@ -0,0 +1,11 @@
+// This is the main function
+fn main() {
+// This is the main function
+    
+    // Print text to the console
+    println!("\x1bc\x1b[42;30m    {}]",rint());
+}
+
+fn rint()->i128{
+    return 0x1234567890123456789;
+}
