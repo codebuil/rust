c20ae020e84f0fbb19a29560392b4d6c258d5b17
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2022-09-28 17:11:00 +0100

    upgrade number

---
+++ toint128.rs
@@ -0,0 +1,12 @@
+// This is the main function
+fn main() {
+// This is the main function
+    
+    // Print text to the console
+    println!("\x1bc\x1b[42;30m    {}]",rint( 0x12345678901 ));
+}
+
+fn rint(i:i64)->i128{
+    let l:i128=i as i128;
+    return l;
+}
