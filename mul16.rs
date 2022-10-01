2936dde273a6bd7283c1377eecde61675d4ab707
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2022-10-01 16:48:26 +0100

    multiple 16 hex i128

---
+++ mul16.rs
@@ -0,0 +1,22 @@
+// This is the main function
+fn main() {
+    // This is the main function
+    let mut _n:i128=0;
+    let mut _m:i128=1;
+        
+    
+     // Print text to the console 
+     println!("\x1bc\x1b[42;30m\n");
+     
+     for _n in 0 .. 128/4-1{
+         println!("d{}",_m);
+         println!("{:x}",_m);
+         _m=mults(_m,16);
+     
+     }
+     
+     } 
+     fn mults(a:i128,b:i128)->i128{
+          return a*b;
+          
+     }
